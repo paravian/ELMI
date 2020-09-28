8a8420628430f7b13471538da91afdd06e1972a6
Tom Brougham <tbrougham@paravian.net>
2020-09-28 10:32:29 +1000

    Update classifyPrepare

--- R/classifyPrepare.R
+++ R/classifyPrepare.R
@@ -1,14 +1,14 @@
 #' Prepare classification data
 #'
 #' This function processes datasets containing numerical and/or discrete value
-#' datasets into a format suitable for analysis using \code{\link{ELSIP}} and
-#' \code{\link{multi_ELSIP}}, and using transformations specifically suited to
-#' each type of data. Presently, three types of data types are supported by
-#' \code{classifyPrepare}: continuous, ratio, and discrete. The variables
-#' desired are specified according to the named columns in \code{data} for
-#' any of the three aforementioned data types. Named columns in
-#' \code{discrete}, \code{continuous} and \code{ratios} will appear in the
-#' returned data frame, with the following transformations applied:
+#' datasets into a format suitable for analysis using \code{\link{ELSIP}} using
+#' transformations specifically suited to each type of data. Presently, three
+#' types of data types are supported by \code{classifyPrepare}: continuous,
+#' ratio, and discrete. The variables desired are specified according to the
+#' named columns in \code{data} for any of the three aforementioned data types.
+#' Named columns in \code{discrete}, \code{continuous} and \code{ratios} will
+#' appear in the returned data frame, with the following transformations
+#' applied:
 #' \itemize{
 #'   \item{\code{discrete} variables are included as is, but converted to
 #'   \code{factor}s;}
@@ -18,7 +18,8 @@
 #'   of the lowest and highest value of the ratio variable, respectively, to
 #'   avoid returning \code{Inf}s.}
 #' }
-#' @importFrom checkmate assert checkChoice checkDataFrame checkCharacter checkSubset
+#' @importFrom checkmate assert assertChoice assertDataFrame assertCharacter
+#'   assertSubset checkCharacter
 #' @importFrom gtools logit
 #' @param data a \code{data.frame} object containing observations.
 #' @param label a character indicating the column in \code{data} to use as the
@@ -28,22 +29,22 @@
 #' @param continuous a character vector of columns representing continuous
 #'   variables.
 #' @param ratios a character vector of columns representing ratio variables.
-#' @return a \code{data.frame} object containing the outcome variable as a
-#'   factor and the transformed variables from \code{data}.
+#' @return An \code{\link{ELSIPData}} object.
 #' @export
 classifyPrepare <- function(data, label = character(), discrete = list(),
                      continuous = list(), ratios = list()) {
-  checkDataFrame(data, min.rows = 1, min.cols = 1)
-  checkChoice(label, names(data))
+  assertDataFrame(data, min.rows = 1, min.cols = 1, col.names = "named")
+  assertCharacter(label, min.len = 1)
+  assertChoice(label, names(data))
 
   assert(
     checkCharacter(discrete, min.len = 1),
     checkCharacter(continuous, min.len = 1),
     checkCharacter(ratios, min.len = 1)
   )
-  checkSubset(discrete, names(data))
-  checkSubset(continuous, names(data))
-  checkSubset(ratios, names(data))
+  assertSubset(discrete, names(data))
+  assertSubset(continuous, names(data))
+  assertSubset(ratios, names(data))
 
   for (d in discrete) {
     data[,d] <- as.factor(data[,d])
@@ -51,9 +52,9 @@ classifyPrepare <- function(data, label = character(), discrete = list(),
   for (c in c(continuous, ratios)) {
     data[,c] <- as.numeric(data[,c])
   }
-  data[,label] <- as.factor(data[,label])
+  y <- as.factor(data[,label])
 
-  usable_cols <- c(which(names(data) == label))
+  x <- c()
 
   if (length(discrete)) {
     if (any(!discrete %in% names(data))) {
@@ -76,7 +77,7 @@ classifyPrepare <- function(data, label = character(), discrete = list(),
     for (var_fact in var_factors) {
       data[,var_fact] <- as.factor(data[,var_fact])
     }
-    usable_cols <- c(usable_cols, discrete_cols)
+    x <- c(x, discrete_cols)
   }
 
   # Transform continuous variables
@@ -88,7 +89,7 @@ classifyPrepare <- function(data, label = character(), discrete = list(),
     cont_cols <- which(names(data) %in% continuous)
 
     data[cont_cols] <- lapply(data[cont_cols], log10)
-    usable_cols <- c(usable_cols, cont_cols)
+    x <- c(x, cont_cols)
   }
 
   # Transform ratio variables
@@ -101,8 +102,8 @@ classifyPrepare <- function(data, label = character(), discrete = list(),
 
     data[ratio_cols] <- lapply(data[ratio_cols],
                                function (r) logit(r, min(r, na.rm = T) - 1, max(r, na.rm = T) + 1))
-    usable_cols <- c(usable_cols, ratio_cols)
+    x <- c(x, ratio_cols)
   }
 
-  return(data[,usable_cols])
+  ELSIPData$new(data[,x], y)
 }
